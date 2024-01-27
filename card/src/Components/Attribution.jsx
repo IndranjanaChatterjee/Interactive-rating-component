531ae93c5e8ea5ada126f2bb93c45211e9f12552
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-01-27 08:26:57 +0530

    fix

--- card/src/Components/Attribution.jsx
+++ card/src/Components/Attribution.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Attribution() {
   return (
-    <div className="attribution text-[#ffffff] m-[0.5rem]">
+    <div className="attribution text-[#ffffff] m-[0.5rem] text-center w-[25rem]">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className='underline'>Frontend Mentor</a>. 
     Coded by <a href="https://www.frontendmentor.io/profile/IndranjanaChatterjee" className='underline'>IndranjanaChatterjee</a>.
   </div>
